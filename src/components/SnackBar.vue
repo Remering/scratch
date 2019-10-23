d0f8354cd2d706b535d373df455a6d780008710f
Remering <1015488424@qq.com>
2019-10-23 21:20:30 +0800

    map v-snackbar to vuex

---
+++ src/components/SnackBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-snackbar :color="color" :value="open" @input="setOpen">
+        {{message}}
+        <v-btn icon @click="close">
+            <v-icon>close</v-icon>
+        </v-btn>
+    </v-snackbar>
+</template>
+
+<script>
+  import {mapMutations, mapState} from 'vuex';
+  const namespace = 'snackbar';
+  export default {
+    name: 'SnackBar',
+    computed: mapState(namespace, [
+        'color', 'message', 'open',
+    ]),
+    methods: mapMutations(namespace, [
+      'close', 'setOpen',
+    ]),
+  };
+</script>
+
+<style scoped>
+
+</style>
