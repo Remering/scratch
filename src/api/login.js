9fbff8117963299fa9c5504e06b7dbd5e14a3b90
Remering <1015488424@qq.com>
2019-10-23 21:19:50 +0800

    add comment for login method

--- src/api/login.js
+++ src/api/login.js
@@ -1,13 +1,14 @@
-import api from './index';
+import { client } from './index';
 
 export default {
-
-  login: async (username, password) =>
-    api.post('/user/login',
-      {
-        username,
-        password,
-      },
+  /**
+   * Send login request to the server
+   * @param userLoginInfo const { username: string, password: string } = userLoginInfo
+   * @returns {Promise<AxiosResponse<T>>}
+   */
+  login: async (userLoginInfo) =>
+    client.post('/user/login',
+      userLoginInfo,
     ),
 
 };
