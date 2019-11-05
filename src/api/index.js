e5d5065e3f64efeb8733358c6028eec78633e2ec
Remering <1015488424@qq.com>
2019-11-05 11:47:41 +0800

    last commit

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,8 @@ import register from '@/api/register';
 import user from '@/api/user';
 
 export const client = axios.create({
-  // baseURL: 'http://49.232.138.118:8080/plarform',
-  baseURL: 'http://localhost:8080/plarform',
+  baseURL: 'http://49.232.138.118:8080/plarform',
+  // baseURL: 'http://localhost:8080/plarform',
 });
 
 export default {
