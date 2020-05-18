f809ec960bfc6d5a2c12c12c096a8f669be0672c
Remering <1015488424@qq.com>
2020-05-18 08:52:17 +0800

    merge all apis

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,20 @@
 import axios from 'axios';
-import login from '@/api/login';
-import register from '@/api/register';
-import user from '@/api/user';
+import account from './account';
+import profile from './profile';
+import file from './file';
+import course from './course';
+import question from './question';
+
 
 export const client = axios.create({
-  baseURL: 'http://49.232.138.118:8080/plarform',
-  // baseURL: 'http://localhost:8080/plarform',
+  // baseURL: 'http://49.232.138.118:8080/plarform',
+  baseURL: 'http://localhost:8888/plarform',
 });
 
 export default {
-  ...login,
-  ...register,
-  ...user,
-}
+  ...account,
+  ...profile,
+  ...file,
+  ...course,
+  ...question
+};
