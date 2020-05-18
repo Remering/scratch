b648d7b3a28d256dbe474c6bbbdfc808b89bd773
Remering <1015488424@qq.com>
2020-05-18 09:04:00 +0800

    change logic

--- src/page/LogoutPage.vue
+++ src/page/LogoutPage.vue
@@ -1,20 +1,17 @@
+<template>
+  <div/>
+</template>
 <script>
-  import {USER_NAMESPACE} from '@/store';
 
   export default {
     name: 'LogoutPage',
     beforeRouteEnter(from, to, next) {
       // eslint-disable-next-line no-console
       next(vm => {
-        vm.$store.dispatch(`${USER_NAMESPACE}/logout`);
-        global.$cookies.keys().forEach(key => global.$cookies.remove(key));
         vm.$router.push('/');
       });
     },
-    beforeRouteUpdate() {
-      window.confirm('2333');
 
-    }
   };
 </script>
 
