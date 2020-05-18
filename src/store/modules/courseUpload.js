018fab11c1d31f381f83dd88be0825f5c1b056c3
Remering <1015488424@qq.com>
2020-05-18 08:40:13 +0800

    change cookie to jwt

--- src/store/modules/courseUpload.js
+++ src/store/modules/courseUpload.js
@@ -1,5 +1,5 @@
 import api from '@/api';
-import {COURSES_NAMESPACE, SNACKBAR_NAMESPACE} from '@/store';
+import {ACCOUNT_NAMESPACE, COURSES_NAMESPACE, SNACKBAR_NAMESPACE} from '@/global';
 
 const state = {
   courseId: -1,
@@ -7,12 +7,14 @@ const state = {
   introduction: '',
   picture: null,
   video: null,
+  file: null
 };
 
 
 const getters = {
   pictureUrl: ({picture}) => picture ? window.URL.createObjectURL(picture) : '',
   videoUrl: ({video}) => video ? window.URL.createObjectURL(video) : '',
+  fileUrl: ({file}) => file ? window.URL.createObjectURL(file) : '',
 };
 
 const mutations = {
@@ -21,22 +23,29 @@ const mutations = {
   setIntroduction: (state, introduction) => state.introduction = introduction,
   setPicture: (state, picture) => state.picture = picture,
   setVideo: (state, video) => state.video = video,
+  setFile: (state, file) => state.file = file,
 };
 
 const actions = {
-  async createCourse({state, dispatch, commit}) {
-    const {name, picture, introduction} = state;
-    const response = await api.createCourse({
+  async createCourse({state, dispatch, commit, rootState}) {
+    const {token} = rootState[ACCOUNT_NAMESPACE];
+    const {name, picture, introduction, video, file} = state;
+
+    const response = await api.createCourse(token, {
       name,
-      picture,
       introduction,
+      picture,
+      video,
+      file
     });
     let {code, message} = response.data;
-    await dispatch(`${SNACKBAR_NAMESPACE}/show${code ? 'Success' : 'Error'}`, message, {
+    await dispatch(`${SNACKBAR_NAMESPACE}/showState`, {
+      code, message
+    }, {
       root: true,
     });
     if (!code) return;
-    const coursesResponse = await api.displayCourse();
+    const coursesResponse = await api.getCourses();
     message = coursesResponse.data.message;
     if (typeof message == 'string') return;
     const course = message.filter(item => item.name === name).first;
@@ -58,10 +67,7 @@ const actions = {
       root: true,
     });
   },
-  async createAndUpload({dispatch}) {
-    await dispatch('createCourse');
-    await dispatch('uploadCourse');
-  }
+
 };
 
 export default {
