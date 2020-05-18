1ad95e3de440f53cff5b353a5b00dbe6640ab3c5
Remering <1015488424@qq.com>
2020-05-18 08:51:57 +0800

    merge all question apis

---
+++ src/api/question/index.js
@@ -0,0 +1,13 @@
+import addQuestion from '@/api/question/addQuestion';
+import answerQuestion from '@/api/question/answerQuestion';
+import deleteQuestion from '@/api/question/deleteQuestion';
+import getQuestions from '@/api/question/getQuestions';
+import modifyText from '@/api/question/modifyText';
+
+export default {
+  addQuestion,
+  answerQuestion,
+  deleteQuestion,
+  getQuestions,
+  modifyText
+};
