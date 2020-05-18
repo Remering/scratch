c58d059eb8418aa5adab2a4a7c11889377f83264
Remering <1015488424@qq.com>
2020-05-18 08:55:26 +0800

    change property name

--- src/components/CourseCard.vue
+++ src/components/CourseCard.vue
@@ -3,11 +3,13 @@
     <v-img :src="imageUrl" aspect-ratio="1.7778" class="yellow--text align-end" max-height="270" max-width="480">
       <v-card-title>{{title}}</v-card-title>
     </v-img>
-    <v-card-text>{{text}}</v-card-text>
+    <v-card-text>{{introduction}}</v-card-text>
   </v-card>
+
 </template>
 
 <script>
+
   export default {
     name: 'CourseCard',
     props: {
@@ -15,19 +17,18 @@
         type: String,
         required: true,
       },
+
       title: {
         type: String,
         required: true,
       },
-      text: {
+      introduction: {
         type: String,
         required: true,
       },
-      // disableClick: {
-      //   type: Boolean,
-      //   required: false,
-      //   default: false,
-      // }
+    },
+    data() {
+      return {};
     }
 
   };
