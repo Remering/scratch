bdb783bab13c2dc4a80da9b7e37b089e04a792c2
Remering <1015488424@qq.com>
2020-05-18 08:45:26 +0800

    merge all api

---
+++ src/api/account/index.js
@@ -0,0 +1,15 @@
+import login from './login';
+import register from './register';
+import logout from './logout';
+import changePassword from './changePassword';
+import findPassword from './findPassword';
+import sendVerificationCode from './sendVerificationCode';
+
+export default {
+  register,
+  login,
+  logout,
+  changePassword,
+  findPassword,
+  sendVerificationCode
+};
