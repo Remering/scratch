af929d8118e2ae0631189c2e723c0599a7ccb298
Remering <1015488424@qq.com>
2020-05-18 08:40:40 +0800

    move global variables to global.js

---
+++ src/global.js
@@ -0,0 +1,20 @@
+export const STUDENT = 0;
+export const TEACHER = 1;
+export const SUCCESS = 0;
+export const INFO = 1;
+export const WARNING = 2;
+export const ERROR = 3;
+
+
+import AvatarDefault from '@/assets/avatar_default.jpg';
+
+export const DEFAULT_AVATAR_URL = AvatarDefault;
+export const COURSES_NAMESPACE = 'courses';
+export const LOGIN_NAMESPACE = 'login';
+export const REGISTER_NAMESPACE = 'register';
+export const SNACKBAR_NAMESPACE = 'snackbar';
+export const ACCOUNT_NAMESPACE = 'account';
+export const AVATAR_UPLOAD_DIALOG_NAMESPACE = 'avatarUploadDialog';
+export const COURSE_UPLOAD_NAMESPACE = 'courseUpload';
+export const VIDEO_PLAY_NAMESPACE = 'videoPlay';
+export const COURSE_MODIFY_NAMESPACE = 'courseModify';
